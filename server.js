d4cab1e23a1781c54a46d9424b38430a2c1ad1c3
Rebecca <beckie04@gmail.com>
2026-01-21 09:21:09 -0800

    feat: add user authentication

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// npm
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -6,17 +7,29 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+// Import routers
+const authRouter = require('./controllers/auth');
+
+const usersRouter = require('./controllers/users');
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
+// Routes
+app.use('/auth', authRouter);
 
+app.use('/users', usersRouter);
+
+// Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log('The express app is ready!');
 });
